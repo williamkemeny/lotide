1ec3ca0bdaf5093a406b8b1516698a241e35fba2
William Kemeny <williamkemeny@gmail.com>
2023-04-22 16:42:33 -0400

    eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,22 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function () {
+  if (arguments[0].length !== arguments[1].length) {
+    return false;
+  }
+  for (let i = 0; i < arguments[0].length; i++) {
+    if (arguments[0][i] !== arguments[1][i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
