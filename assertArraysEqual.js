b2e5b73bebb7ca9ae3d20ee8fe6a5d3d283905e0
William Kemeny <williamkemeny@gmail.com>
2023-04-22 16:59:48 -0400

    assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const eqArrays = function () {
+  if (arguments[0].length !== arguments[1].length) {
+    return false;
+  }
+  for (let i = 0; i < arguments[0].length; i++) {
+    if (arguments[0][i] !== arguments[1][i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (a, b) =>
+  console.log(
+    `We can say that the inputs ${a} and ${b} being equal is ${eqArrays(a, b)}.`
+  );
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
