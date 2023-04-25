f94b94a75894c9499748e79ba5f480c84c64daa4
William Kemeny <williamkemeny@gmail.com>
2023-04-25 17:55:06 -0400

    countLetter.js function

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (string) {
+  let letterCount = {};
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === " ") {
+    } else if (letterCount[string[i]] === undefined) {
+      letterCount[string[i]] = 1;
+    } else {
+      letterCount[string[i]]++;
+    }
+  }
+
+  return letterCount;
+};
+
+console.log(countLetters("L H L"));
