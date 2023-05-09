e26533fbb9ce736206d1a974e3c99b026d77c8c4
William Kemeny <williamkemeny@gmail.com>
2023-05-09 18:24:19 -0400

    Test files created and refactored some code

--- takeUntil.js
+++ takeUntil.js
@@ -1,5 +1,3 @@
-// const assertArraysEqual = require("./assertArraysEqual");
-
 const takeUntil = function (array, callback) {
   if (Array.isArray(array) !== true) {
     return "This is not an array.";
@@ -14,25 +12,5 @@ const takeUntil = function (array, callback) {
   }
   return result;
 };
-
-// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-// const results1 = takeUntil(data1, (x) => x < 0);
-// assertArraysEqual(results1, [1, 2, 5, 7, 2]);
-
-// console.log("---");
-
-// const data2 = [
-//   "I've",
-//   "been",
-//   "to",
-//   "Hollywood",
-//   ",",
-//   "I've",
-//   "been",
-//   "to",
-//   "Redwood",
-// ];
-// const results2 = takeUntil(data2, (x) => x === ",");
-// assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
 
 module.exports = takeUntil;
